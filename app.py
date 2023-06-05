fbef2dea404f5a18665563c38a1e8163b8c14434
Joel Becker <joel_becker@mail.com>
2023-06-05 21:30:27 +0100

    multi rows and hide default format

--- app.py
+++ app.py
@@ -32,35 +32,46 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
+
 st.title("Financial Life Model")
 
-# Create columns for parameter inputs
-left, center, right = st.columns(3)
+# Create a first row of sliders
+row1_space1, row1_space2 = st.columns(2)
 
-# Sliders for the model's parameters
-with left:
+with row1_space1:
     m = st.slider("Number of paths", 100, 500, 300)
     years = st.slider("Number of years", 10, 50, 25)
 
-with center:
-    #st.markdown("### Income lifecycle")
+with row1_space2:
     cash_start = st.slider("Initial cash", 0, 100_000, 10_000)
     market_start = st.slider("Initial market wealth", 0, 500_000, 30_000)
     income_start = st.slider("Initial income", 20_000, 200_000, 40_000)
-    #st.markdown("### Income AR(1) coefficients")
 
-with right:
-    min_income = st.slider(
-        "Minimum income",
-        0,
-        50_000,
-        10_000,
-        help="The reservation income, a lower bound that you don't expect to dip below.",
-    )
-    inflation_rate = st.slider("Inflation rate", 0.0, 0.1, 0.02)
-    r = st.slider(
-        "Interest rate", 0.0, 0.1, 0.02, help="The rate of return on savings."
-    )
+# Create a collapsible section for the second row
+with st.expander("Advanced Settings"):
+    row2_space1, row2_space2 = st.columns(2)
+
+    with row2_space1:
+        min_income = st.slider(
+            "Minimum income",
+            0,
+            50_000,
+            10_000,
+            help="The reservation income, a lower bound that you don't expect to dip below.",
+        )
+        inflation_rate = st.slider("Inflation rate", 0.0, 0.1, 0.02)
+
+    with row2_space2:
+        r = st.slider(
+            "Interest rate", 0.0, 0.1, 0.02, help="The rate of return on savings."
+        )
 
 variables_to_plot = st.multiselect(
     "Select variables to plot",
