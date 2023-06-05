ce8dd68510c868786648bdcc083d6bcb6e7a4e50
Joel Becker <joel_becker@mail.com>
2023-06-05 20:13:53 +0100

    add test; change requirements w new env

---
+++ test_utilities.py
@@ -0,0 +1,23 @@
+import utilities
+import parameters as params
+
+def test_financial_life_model():
+    # Create a parameters dictionary
+    test_params = params.input_params
+    test_params["m"] = 300
+    test_params["years"] = 25
+    test_params["cash_start"] = 10000
+    test_params["market_start"] = 30000
+    test_params["life_cycle_income"] = [
+        60000 + (year * 3 * 1000) for year in range(25)
+    ]
+    test_params["min_income"] = 10000
+    test_params["inflation_rate"] = 0.02
+    test_params["r"] = 0.02
+
+    # Run your model
+    model_output = utilities.financial_life_model(test_params)
+
+    # Assert something about the output
+    assert len(model_output) > 0, "Model output is empty"
+
