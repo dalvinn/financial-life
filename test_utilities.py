ec95dcf54dbf82ebe2b979eb75955a87a4d5e4ca
joel-becker <joelhbkr@gmail.com>
2023-07-29 21:44:48 +0200

    fix years vs baseline a different way -- vs retirement

--- test_utilities.py
+++ test_utilities.py
@@ -5,11 +5,11 @@ def test_financial_life_model():
     # Create a parameters dictionary
     test_params = params.input_params
     test_params["m"] = 300
-    test_params["years"] = 25
+    test_params["years"] = test_params["years_until_retirement"] + 10
     test_params["cash_start"] = 10000
     test_params["market_start"] = 30000
     test_params["life_cycle_income"] = [
-        60000 + (year * 3 * 1000) for year in range(25)
+        60000 + (year * 3 * 1000) for year in range(test_params["years"])
     ]
     test_params["min_income"] = 10000
     test_params["inflation_rate"] = 0.02
